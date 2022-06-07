525c0142f4c7be1d06ccdfea91fdea9aed3de7e6
griffin-schrauth <griffinschrauth@gmail.com>
2022-06-07 16:38:45 -0400

    setup database
    with some minor code in controller

---
+++ API/Cards/Cards.API/Data/CardsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cards.API.Models;
+
+namespace Cards.API.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Card> Cards {get;set;}
+    }
+}
